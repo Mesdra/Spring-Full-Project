91db5e65f35964f710759fb8a1d76a479fca955a
Mesdra <vinicavalari97@gmail.com>
2019-07-16 14:40:37 -0300

    projeto criado

---
+++ src/main/java/com/mesdra/SpringProject/Resource/CategoriaResource.java
@@ -0,0 +1,19 @@
+package com.mesdra.SpringProject.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public String listar() {
+		return "rest esta funcionando";
+	}
+
+}
---
+++ src/main/java/com/mesdra/SpringProject/SpringFullProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mesdra.SpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFullProjectApplication.class, args);
+	}
+
+}
