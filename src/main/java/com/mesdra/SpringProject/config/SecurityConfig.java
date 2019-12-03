139a8022058b39594a0fda0f79569f5b27ae9b57
Mesdra <vinicavalari97@gmail.com>
2019-12-02 21:55:18 -0300

    implementando autorização

--- src/main/java/com/mesdra/SpringProject/config/SecurityConfig.java
+++ src/main/java/com/mesdra/SpringProject/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mesdra.SpringProject.security.JWTAuthenticationFilter;
+import com.mesdra.SpringProject.security.JWTAuthorizationFilter;
 import com.mesdra.SpringProject.security.JWTUtil;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
